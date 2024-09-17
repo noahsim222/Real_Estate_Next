6f8b7de7e06b825954a738512f388c0b937d3e01
noahsim222 <noahsim222@gmail.com>
2024-09-17 18:19:02 +0900

    complete

--- app/components/homeownership.tsx
+++ app/components/homeownership.tsx
@@ -4,20 +4,20 @@ import React, { useState, useEffect, useRef } from "react";
 import Image from "next/image"
 
 const Homeownership: React.FC = () => {
-  const [elementHeight, setElementHeight] = useState<number | undefined>(undefined);
-  const elementRef = useRef<HTMLDivElement>(null)
+  const [elementHeight1, setElementHeight1] = useState<number | undefined>(undefined);
+  const elementRef1 = useRef<HTMLDivElement>(null)
 
-  const updateHeight = () => {
-    if (elementRef.current) {
-      setElementHeight(elementRef.current.offsetHeight)
+  const updateHeight1 = () => {
+    if (elementRef1.current) {
+      setElementHeight1(elementRef1.current.offsetHeight)
     }
   }
 
   useEffect(() => {
-    updateHeight();
-    window.addEventListener('resize', updateHeight)
+    updateHeight1();
+    window.addEventListener('resize', updateHeight1)
     return () => {
-      window.removeEventListener('resize', updateHeight)
+      window.removeEventListener('resize', updateHeight1)
     }
   }, [])
 
@@ -26,7 +26,7 @@ const Homeownership: React.FC = () => {
       <h1 className="text-4xl font-bold pt-10 px-5 text-center max-[540px]:text-3xl">Discover Homeownership on</h1>
       <p className="text-[25px] text-[#a3a4a8] mt-5 px-5 text-center max-[540px]:text-xl">Renting is great, but maybe you&apos;re thinking about buying a home instead. We want you to find the right place. That&apos;s why our sister site, Homes.com, is designed to help you find your dream home, even if you&apos;re a first time buyer.</p>
       <div className="mt-20 lg:flex justify-center items-center p-5">
-        <div className="basis-1/2 p-16 grid gap-4 bg-black bg-opacity-5 rounded-t-lg lg:rounded-l-lg " ref={elementRef}>
+        <div className="basis-1/2 p-16 grid gap-4 bg-black bg-opacity-5 rounded-t-lg lg:rounded-l-lg " ref={elementRef1}>
           <p className="text-[30px] text-black max-[540px]:text-[25px] max-[540px]:text-center">Explore Your Options</p>
           <p className="text-[20px] text-[#404042] max-[540px]:text-[20px] max-[540px]:text-center">Deciding to become a homeowner is a big deal! Luckily, with Homes.com, you get the most accurate homes for sale property data, an agent directory, and collaboration tools to browse with your agent and co-shopper to help you make the right decision.</p>
           <button type="button" className="text-[35px] text-[#67709d] hover:text-[#92949c] text-left max-[540px]:text-[25px] max-[540px]:text-center">
@@ -40,7 +40,7 @@ const Homeownership: React.FC = () => {
             width={746}
             height={340}
             className="w-full rounded-b-lg lg:rounded-r-lg  max-h-[468px]"
-            style={{height: `${elementHeight}px`}}
+            style={{height: `${elementHeight1}px`}}
           />
         </div>
       </div>
